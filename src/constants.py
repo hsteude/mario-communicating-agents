f77f762748e2ec37dd4f624ca3bda3fbe4052e75
Henrik <henrik.steude@ginkgo-analytics.com>
2020-12-22 11:30:59 +0100

    question answer

--- src/constants.py
+++ src/constants.py
@@ -5,6 +5,7 @@ import os
 DATA_ROOT_PATH = 'data'
 IMGS_SERIES_FOLDER_PATH = os.path.join(DATA_ROOT_PATH, 'imgs_series')
 LABELS_TABLE_PATH = os.path.join(DATA_ROOT_PATH, 'labels_table.csv')
+LABELS_TABLE_QA_PATH = os.path.join(DATA_ROOT_PATH, 'labels_table_qa.csv')
 
 
 # random hidden states
@@ -12,8 +13,12 @@ box_x = np.random.uniform(low=300, high=800, size=1)
 pipe_x = np.random.uniform(low=900, high=1100, size=1)
 enemy_speed = np.random.choice(range(25, 50))
 
-# 
-HIDDEN_STATE_COLS = ['imgs_folder_path', 'box_x', 'pipe_x', 'enemy_speed']
-OPT_ANSWER_SUFFIX = '_opt_answer'
+# random question parameters (marios speed)
+MARIO_SPEED_MIN = 50
+MARIO_SPEED_MAX = 100
 
-
+# columns and stuff
+IMGS_PATH_COL = 'imgs_folder_path'
+HIDDEN_STATE_COLS = ['box_x', 'pipe_x', 'enemy_speed']
+QUESTION_COL = 'mario_speed'
+ANSWER_COLS = ['answer_box', 'answer_pipe', 'answer_enemy']
