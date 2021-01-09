041becf99f35372d8c270cdd95b46ca7cffe1f0b
Henrik <henrik.steude@ginkgo-analytics.com>
2021-01-09 22:55:14 +0000

    saturday night

--- utils/run_training_gpus.sh
+++ utils/run_training_gpus.sh
@@ -11,15 +11,15 @@ python ./scripts/model_training.py \
         --dec_num_hidden_layers=10 \
         --dec_single_answer_dim=1 \
         --filt_initial_log_var=-10 \
-        --pretrain_thres=0.01 \
+        --pretrain_thres=0.2 \
         --beta=0.001 \
-        --learning_rate=0.0001 \
+        --learning_rate=0.0005 \
         --batch_size=32 \
-        --dl_num_workers=32 \
+        --dl_num_workers=8 \
         --validdation_split=0.05 \
         --max_epochs=1000 \
         --logger=True \
-        --gpus=4 \
+        --gpus=1 \
         --accelerator=ddp \
         --no-enc_fixed_cnn_weight \
         --enc_pretrained 
